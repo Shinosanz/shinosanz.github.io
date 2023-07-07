3c1ddff0b3a4403a2d66da5a64a4ffe2fc87f75e
Shinosanz <tonza2544@hotmail.com>
2023-07-07 14:15:26 +0700

    Preloader Added
    To Solve :
    set delay for preloader

---
+++ script.js
@@ -0,0 +1,27 @@
+// Preloader
+
+var loader;
+
+setTimeout(() => { console.log("World!"); }, 5000);
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.05);
+        }, 50);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('homepage').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    loader = document.getElementById('loader');
+    loadNow(1);
+    
+});
